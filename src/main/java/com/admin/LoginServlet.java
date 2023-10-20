18f0b05f1a649a50e4250f18f22cc12383405880
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2023-10-20 20:09:23 +0530

    admin

---
+++ src/main/java/com/admin/LoginServlet.java
@@ -0,0 +1,70 @@
+package com.admin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		
+		
+		String username =   request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		boolean isTrue;
+		
+		isTrue = adminDbUtill.validate(username, password);
+	
+		
+	
+	
+		if(isTrue == true) {
+			
+		
+			
+			
+			
+			HttpSession session = request.getSession();
+            session.setAttribute("isLoggedIn", true);
+            
+            List<admin> adminDetails = adminDbUtill.getAdminDetails(username);
+			request.getSession().setAttribute("adminDetails", adminDetails);
+            
+            RequestDispatcher dis =request.getRequestDispatcher("index.jsp");
+    		dis.forward(request,response);
+            
+            
+		}
+		else {
+			
+			out.println("<script type='text/javascript'>");
+			out.println("alert('Your username or password is incorrect');");
+			out.println("location='login.jsp'");
+			out.println("</script>");
+		}
+	
+		
+		
+		
+		
+		
+
+		 
+	}
+
+}
