4ddc0be1fbd8dd8a710a867f93877f1cd6381a12
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2023-10-23 13:24:13 +0530

    update

--- src/main/java/com/admin/DBconnect.java
+++ src/main/java/com/admin/DBconnect.java
@@ -6,9 +6,11 @@ import java.sql.DriverManager;
 public class DBconnect {
 	
 	 //create database connection
+
     private static String url = "jdbc:mysql://localhost:3306/hotel?autoReconnect=true&useSSL=false";
     private static String user = "root";
     private static String passwd = "G336";
+
     private static Connection con;
     
     public static Connection getConnetion() {
@@ -17,8 +19,12 @@ public class DBconnect {
     		Class.forName("com.mysql.jdbc.Driver");
     		
     		 con = DriverManager.getConnection(url, user, passwd);
+
 		} catch (Exception e) {
 			e.printStackTrace();
+
+    		 
+		
 		}
     	
     	return con;
